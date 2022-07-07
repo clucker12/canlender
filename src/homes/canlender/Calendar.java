558689aa762f43a241e3dca0d57f2fe8aaec5602
seong ho <412jsh@naver.com>
2022-07-07 20:20:36 +0900

    while문 이용

--- src/homes/canlender/Calendar.java
+++ src/homes/canlender/Calendar.java
@@ -17,16 +17,26 @@ public class Calendar {
     }
     public static void main(String[] args){
 
-
         //숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
+
+        String PROMPT="cal> ";
         Scanner scanner = new Scanner(System.in);
         Calendar cal = new Calendar();//생성자 선언
-        System.out.println("반복횟수를 입력하세요.");
-        int count=scanner.nextInt();
-        for(int i=0;i<count;i++){
+//        System.out.println("반복횟수를 입력하세요.");
+//        int count=scanner.nextInt();
+        int month=0;
+        while (month !=-1){
             System.out.println("월을 입력하세요");
-            int month = scanner.nextInt();
-            System.out.printf("%d월은 %d일까지 있습니다.\n", month,cal.maxDaysOfMonth(month));
+            System.out.print(PROMPT);
+            month = scanner.nextInt();
+
+            if(month==-1){
+                System.out.println("Have a nice day!");
+            }else if(month>12){
+                continue;
+            }else{
+                System.out.printf("%d월은 %d일까지 있습니다.\n", month,cal.maxDaysOfMonth(month));
+            }
         }
 
         cal.printSempleCalendar();
